4b41bafaa9688f9a59e45e4219b7858814defdbd
Won <andrew112517@naver.com>
2025-04-18 15:30:48 +0900

    [feat] UIViewController+
    화면 터치시 키보드 내리는 코드 추가

---
+++ TVING/TVING/Global/Extension/UIViewController+.swift
@@ -0,0 +1,21 @@
+//
+//  UIViewController+.swift
+//  TVING
+//
+//  Created by 김승원 on 4/17/25.
+//
+
+import UIKit
+
+extension UIViewController {
+    /// 화면 터치시 키보드를 내리는 함수입니다
+    func hideKeyboardWhenDidTap() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        tapGesture.cancelsTouchesInView = false
+        view.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
