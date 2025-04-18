8eadae70b683092817d56bff13490bc6cf185fe9
Won <andrew112517@naver.com>
2025-04-18 20:47:09 +0900

    [feat] UIButton+
    버튼 밑줄 추가

---
+++ TVING/TVING/Global/Extension/UIButton+.swift
@@ -0,0 +1,21 @@
+//
+//  UIButton+.swift
+//  TVING
+//
+//  Created by 김승원 on 4/18/25.
+//
+
+import UIKit
+
+extension UIButton {
+    /// 버튼 title에 밑줄을 추가하는 함수입니다
+    func addUnderline() {
+        let attributedString = NSMutableAttributedString(string: self.titleLabel?.text ?? "")
+        attributedString.addAttribute(
+            .underlineStyle,
+            value: NSUnderlineStyle.single.rawValue,
+            range: NSRange(location: 0, length: attributedString.length)
+        )
+        setAttributedTitle(attributedString, for: .normal)
+    }
+}
