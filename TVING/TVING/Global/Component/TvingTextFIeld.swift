1c7d7a6f10cf4af6a9344db3a31a9558e41f7f0e
Won <andrew112517@naver.com>
2025-04-19 16:49:43 +0900

    [refactor] TvingTextField에 .nickName 추가

--- TVING/TVING/Global/Component/TvingTextFIeld.swift
+++ TVING/TVING/Global/Component/TvingTextFIeld.swift
@@ -28,20 +28,20 @@ final class TvingTextField: UITextField {
 
 private extension TvingTextField {
     func setTextField(textFieldType: TextFieldType) {
-        placeholder = textFieldType.placeholder
-        textColor = .gray2
-        font = .pretendard(.semiBold, size: 15)
-        tintColor = .primaryRed
-        backgroundColor = .gray4
-        
         configureDefaultTextField()
         addPadding(left: 22)
-        setPlaceholderColor(.gray2)
         setCornerRadius(3)
         setBorder(0, borderColor: .gray2)
         
+        tintColor = .primaryRed
+        font = textFieldType.font
+        textColor = textFieldType.textColor
+        backgroundColor = textFieldType.backgroundColor
+        placeholder = textFieldType.placeholder
+        setPlaceholderColor(textFieldType.placeholderColor)
         isSecureTextEntry = textFieldType.isSecureTextEntry
         rightViewMode = textFieldType.rightViewMode
+        keyboardType = textFieldType.keyboardType
     }
 }
 
@@ -49,13 +49,59 @@ extension TvingTextField {
     enum TextFieldType {
         case id
         case password
+        case nickname
         
+        var font: UIFont {
+            switch self {
+            case .id:
+                return .pretendard(.semiBold, size: 15)
+            case .password:
+                return .pretendard(.semiBold, size: 15)
+            case .nickname:
+                return .pretendard(.regular, size: 14)
+            }
+        }
+        
+        var textColor: UIColor {
+            switch self {
+            case .id:
+                return .gray2
+            case .password:
+                return .gray2
+            case .nickname:
+                return .primaryBlack
+            }
+        }
+        
+        var backgroundColor: UIColor {
+            switch self {
+            case .id:
+                return .gray4
+            case .password:
+                return .gray4
+            case .nickname:
+                return .gray1
+            }
+        }
         var placeholder: String {
             switch self {
             case .id:
                 return "아이디"
             case .password:
                 return "비밀번호"
+            case .nickname:
+                return "닉네임"
+            }
+        }
+        
+        var placeholderColor: UIColor {
+            switch self {
+            case .id:
+                return .gray2
+            case .password:
+                return .gray2
+            case .nickname:
+                return .gray2
             }
         }
         
@@ -65,6 +111,8 @@ extension TvingTextField {
                 false
             case .password:
                 true
+            case .nickname:
+                false
             }
         }
         
@@ -74,6 +122,19 @@ extension TvingTextField {
                 return .whileEditing
             case .password:
                 return .whileEditing
+            case .nickname:
+                return .whileEditing
+            }
+        }
+        
+        var keyboardType: UIKeyboardType {
+            switch self {
+            case .id:
+                return .emailAddress
+            case .password:
+                return .default
+            case .nickname:
+                return .default
             }
         }
     }
