2d7e4934d122367227266c4899a718470906780f
Won <andrew112517@naver.com>
2025-05-31 18:48:18 +0900

    [feat] filterSection 구현

--- TVING_SwiftUI/TVING_SwiftUI/HomeView.swift
+++ TVING_SwiftUI/TVING_SwiftUI/HomeView.swift
@@ -8,52 +8,9 @@
 import SwiftUI
 
 struct HomeView: View {
-    
-    // MARK: - body
-    
     var body: some View {
-        VStack(alignment: .center, spacing: 0) {
-            titleSection
-            
-            Spacer()
-        }
-        .background(.coreBlack)
-    }
-    
-    // MARK: - Subviews
-    
-    var titleSection: some View {
-        HStack(alignment: .center, spacing: 0) {
-            Image(.tvingTitle)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 191, height: 78)
-            
-            Spacer()
-            
-            Button {
-                print("검색 버튼")
-            } label: {
-                Image(.magnifyingGlass)
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 30, height: 30)
-            }
-            .buttonStyle(.plain)
-            
-            Button {
-                print("프로필 버튼")
-            } label: {
-                Image(.profile)
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 30, height: 30)
-            }
-            .buttonStyle(.plain)
-            .padding(.leading, 10)
-            
-        }
-        .padding(.trailing, 10)
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .foregroundStyle(.coreWhite)
     }
 }
 
